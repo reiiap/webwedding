74bb4c1903bf63bb2d99043ebc4edab903d2b175
ReiiAp <raihanalena@gmail.com>
2026-06-21 17:12:20 +0700

    Refine premium storytelling landing page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,15 @@
-import './globals.css';
-import { buildMetadata, organizationJsonLd, websiteJsonLd } from '../lib/seo.js';
-
+import "./globals.css";
+import {
+  buildMetadata,
+  organizationJsonLd,
+  websiteJsonLd,
+} from "../lib/seo.js";
 
 export const metadata = buildMetadata();
 export const viewport = {
-  width: 'device-width',
+  width: "device-width",
   initialScale: 1,
-  themeColor: '#050505'
+  themeColor: "#050505",
 };
 
 export default function RootLayout({ children }) {
@@ -14,13 +17,22 @@ export default function RootLayout({ children }) {
     <html lang="id">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@500;600;700&family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify([organizationJsonLd, websiteJsonLd]) }}
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify([organizationJsonLd, websiteJsonLd]),
+          }}
         />
         {children}
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,39 +1,53 @@
-import Script from 'next/script';
-import { readHtmlBody } from '../lib/html.js';
-import { buildCanonical, buildMetadata, defaultDescription, defaultTitle, siteUrl } from '../lib/seo.js';
+import Script from "next/script";
+import { readHtmlBody } from "../lib/html.js";
+import {
+  buildCanonical,
+  buildMetadata,
+  defaultDescription,
+  defaultTitle,
+  siteUrl,
+} from "../lib/seo.js";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 export const metadata = buildMetadata({
   title: defaultTitle,
   description: defaultDescription,
-  path: '/'
+  path: "/",
 });
 
 const pageJsonLd = {
-  '@context': 'https://schema.org',
-  '@type': 'WebPage',
-  '@id': `${siteUrl}/#home`,
-  url: buildCanonical('/'),
+  "@context": "https://schema.org",
+  "@type": "WebPage",
+  "@id": `${siteUrl}/#home`,
+  url: buildCanonical("/"),
   name: defaultTitle,
   description: defaultDescription,
-  isPartOf: { '@id': `${siteUrl}/#website` },
-  about: { '@id': `${siteUrl}/#business` },
+  isPartOf: { "@id": `${siteUrl}/#website` },
+  about: { "@id": `${siteUrl}/#business` },
   primaryImageOfPage: `${siteUrl}/assets/hero-preview.svg`,
-  inLanguage: 'id-ID',
+  inLanguage: "id-ID",
   mainEntity: {
-    '@type': 'Service',
-    name: 'Jasa undangan digital online murah',
-    provider: { '@id': `${siteUrl}/#business` },
-    areaServed: 'Indonesia',
-    serviceType: ['undangan digital murah', 'undangan online', 'undangan digital pernikahan', 'undangan website wedding']
-  }
+    "@type": "Service",
+    name: "Jasa undangan digital premium",
+    provider: { "@id": `${siteUrl}/#business` },
+    areaServed: "Indonesia",
+    serviceType: [
+      "undangan digital premium",
+      "undangan online",
+      "undangan digital pernikahan",
+      "undangan website wedding",
+    ],
+  },
 };
 
 export default function HomePage() {
   return (
     <>
-      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(pageJsonLd) }} />
-      <div dangerouslySetInnerHTML={{ __html: readHtmlBody('index.html') }} />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(pageJsonLd) }}
+      />
+      <div dangerouslySetInnerHTML={{ __html: readHtmlBody("index.html") }} />
       <Script src="/script.js" strategy="afterInteractive" />
     </>
   );
